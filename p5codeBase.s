22a19fb72ce4f06dc5c7fa62b72b44ffe683f374
Cyrus Riahi <cyrusriahi@gmail.com>
2018-04-26 14:07:14 -0700

    compiles in qt spim. getting expected values correctly. resolved crash with improper placement of  definition

--- p5codeBase.s
+++ p5codeBase.s
@@ -9,19 +9,86 @@
 # int getDigit(int number);
 # List Used Registers Here:
 #
+# $a0 -> number
+# $t0 -> sum
+# $t1 -> 10
+# $t2 -> temp for remainder
+# $t3 -> temp for quotient
+#
 # #
 getDigit:
 
+li $t0, 0
+li $t1, 10
+bge $a0, $t1, else1
+add $t0, $0, $a0
+j endGetDigit
+else1:
+rem $t2, $a0, $t1
+div $t3, $a0, $t1
+add $t0, $t2, $t3
+endGetDigit:
+move $v0, $t0
+jr $ra
 
 
-##
+
+
+
+# #
 # int sumOfDoubleEvenPlace(int number);
 # List Used Registers Here:
 # sum --> $s0
 # digit --> $s1
+# $t8 --> copy of number arg
+# $t9 --> temp for digit*2
 # 
-##
+# #
 sumOfDoubleEvenPlace:
+addi $sp, $sp, -20
+sw $s3, ($sp)
+sw $s2, 4($sp)
+sw $s1, 8($sp)
+sw $s0, 12($sp)
+sw $ra, 16($sp)
+
+
+li $s0, 0 # sum
+li $s1, 0 # digit
+li $t8, 0 # number
+li $t1, 10 # 10
+move $t8, $a0
+div $t8, $t8, $t1
+
+ble $t8, $0, function_endwhile
+function_while:
+rem $s1, $t8, $t1 # digit = (number % 10);
+mul $t9, $s1, 2 # temp for digit*2
+
+move $a0, $t9 # set up args for getDigit
+jal getDigit # call getDigit
+add $s0, $s0, $v0 # sum += getDigit's return
+
+div $t8, $t8, $t1
+div $t8, $t8, $t1 # didn't want to make another variable for 100, so i divided by 10 twice
+
+bgt $t8, $t0, function_while
+
+function_endwhile:
+move $v0, $s0
+
+
+
+lw $ra, 16($sp)
+lw $s0, 12($sp)
+lw $s1, 8($sp)
+lw $s2, 4($sp)
+lw $s3, ($sp)
+addi $sp, $sp, 20
+
+jr $ra
+
+
 
 main:
   li $s0, 89744563  # int test1 = 89744563;
